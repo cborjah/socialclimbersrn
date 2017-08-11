605f4277fa6513e158a606804b2fb68820caad1e
Christian Borja <cpborja32@gmail.com>
2017-08-11 15:30:16 -0700

    sets up eslint and project file structure

---
+++ app/screens/Login.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Login = () => (
+  <View>
+    <Text>Welcome to Social Climbers</Text>
+  </View>
+);
+
+export default Login;
